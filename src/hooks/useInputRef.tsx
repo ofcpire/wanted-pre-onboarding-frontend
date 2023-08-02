b12c7a162dcab2125d912ffc6b9758c154d6ecc8
ofcpire <ofcpire@gmail.com>
2023-08-02 20:28:27 +0900

    :sparkles: 커스텀훅 useInputRef 작성

---
+++ src/hooks/useInputRef.tsx
@@ -0,0 +1,31 @@
+import { useState, useEffect, ChangeEvent,useRef } from 'react';
+
+export default function useInputRef () {
+  const emailRef=useRef<HTMLInputElement | null>(null);
+  const pwRef=useRef<HTMLInputElement | null>(null);
+  const [emailvalid,setEmailvalid]=useState(false);
+  const [pwValid,setPwValid]=useState(false);
+  const [pass,setPass]=useState(false);
+
+  const emailChangeHandler=(e:ChangeEvent<HTMLInputElement>)=>{
+    if(emailRef.current) {
+      emailRef.current.value=e.target.value;
+      if(emailRef.current.value.includes('@')) setEmailvalid(true);
+      else setEmailvalid(false);
+    }
+  }
+  const pwChangeHandler=(e:ChangeEvent<HTMLInputElement>)=>{
+    if(pwRef.current) {
+      pwRef.current.value=e.target.value;
+      if(pwRef.current.value.length>=8) setPwValid(true);
+      else setPwValid(false);
+    }
+  }
+
+  useEffect(()=>{
+    setPass(emailvalid&&pwValid);
+  },[emailvalid,pwValid])
+
+  return {emailRef,emailChangeHandler,pwRef,pwChangeHandler,pass};
+}
+
