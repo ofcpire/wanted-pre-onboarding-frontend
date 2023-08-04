2d0d1342c7e7f5c79d315808d9b37281fd5db62d
ofcpire <ofcpire@gmail.com>
2023-08-04 14:06:01 +0900

    :sparkles: Signin page, signup page에서 토큰유무 확인 및 리다이렉트 기능

--- src/pages/SigninPage.tsx
+++ src/pages/SigninPage.tsx
@@ -1,6 +1,15 @@
+import {useEffect} from 'react';
+import { getToken } from '../lib/utils/token';
+import { useNavigate } from 'react-router-dom';
 import SigninForm from "../components/input/SigninForm";
 
 export default function SigninPage() {
+  const navigate=useNavigate();
+
+  useEffect(()=>{
+    if(getToken()) navigate('/todo');
+  },[])
+
   return (
     <SigninForm/>
   );
--- src/pages/SignupPage.tsx
+++ src/pages/SignupPage.tsx
@@ -1,6 +1,15 @@
+import {useEffect} from 'react';
+import { getToken } from '../lib/utils/token';
+import { useNavigate } from 'react-router-dom';
 import SignupForm from "../components/input/SignupForm"
 
 export default function SignupPage() {
+  const navigate=useNavigate();
+
+  useEffect(()=>{
+    if(getToken()) navigate('/todo');
+  },[])
+
   return(
     <SignupForm/>
   )
